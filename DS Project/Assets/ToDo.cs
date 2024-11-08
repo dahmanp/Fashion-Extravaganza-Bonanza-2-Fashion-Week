1066328d744b15c44c1d674db60c3ef7f6cc3df7
dahmanp <dahmanp@my.erau.edu>
2024-11-08 12:37:52 -0700

    Clean Up Code & Assets (1)

--- DS Project/Assets/ToDo.cs
+++ DS Project/Assets/ToDo.cs
@@ -1,15 +1,9 @@
 // TO DO LIST:
 
-//UI:
-    //- Main Menu BG
-    //- theme title card
-
 //3D Assets:
-    //- New MC
     //- New clothing
 
 //Code:
-    //- Switch out all arrays for circular linked lists
     //- Point system: each clothing piece has an associated number
     //- New preset outfits
     //- randomize theme code
